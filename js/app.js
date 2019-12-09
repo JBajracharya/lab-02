c1a8d90aa728d951373f505fef61be2c84e2997e
Bajracharya <bajracj@38f9d3dac8ef.ant.amazon.com>
2019-12-08 20:04:14 -0800

    added style, options

---
+++ js/app.js
@@ -0,0 +1,96 @@
+'use strict';
+
+const options = [];
+
+function Horn(img_url, title, description, keyword, horn) {
+	this.img_url = img_url;
+	this.title = title;
+	this.description = description;
+	this.keyword = keyword;
+	this.horn = horn;
+}
+
+Horn.prototype.renderWithJquery = function() {
+	$('#photo-template').append(`
+    <div>
+        <h2> ${this.title}</h2>
+        <img src="${this.img_url}" />
+    </div>
+    `);
+};
+
+Horn.prototype.renderWithJqueryClone = function() {
+	let clone = $('#photo-template').clone();
+
+	clone.find('h2').text(this.title);
+    clone.find('img').attr('src', this.img_url);
+    clone.attr('id', `${this.keyword}`);
+	// clone.removeAttr('id');
+
+	$('#horns').append(clone);
+};
+
+Horn.prototype.renderOptions = function() {
+	if (!options.includes(this.keyword)) {
+		options.push(this.keyword);
+		$('#keywords').append(`
+        <option value=${this.keyword} > ${this.keyword}</option>
+        `);
+		// console.log('key',this.keyword);
+	}
+};
+
+Horn.prototype.renderKeywordImages = function() {
+	$('#keywords').on('click', function() {
+		let selectedItem = $('#keywords').val();
+		console.log(1);
+		if (selectedItem === this.keyword) {
+			this.renderWithJquery();
+		}
+	});
+};
+
+$.get('data/page-1.json').then((data) => {
+	data.forEach((objFromJsonFile) => {
+		let horn = new Horn(
+			objFromJsonFile.image_url,
+			objFromJsonFile.title,
+			objFromJsonFile.description,
+			objFromJsonFile.keyword,
+			objFromJsonFile.horn
+		);
+		// console.log(horn);
+		// console.log(objFromJsonFile.img_url);
+		// console.log(objFromJsonFile.title);
+		// debugger;
+		horn.renderWithJqueryClone();
+		horn.renderOptions();
+		horn.renderKeywordImages();
+	});
+});
+
+// // function filterHornImg() {
+//     $('select').on('change', function() {
+//       let selectedKeyword = $(this).val();
+//       if(selectedKeyword !== 'default') {
+//         $('section').hide();
+//         $(`section[class = "${selectedKeyword}"]`).show();
+//       } else {
+//         $('section').show();
+//       }
+//     });
+// //   }
+// //   filterHornImg();
+
+$('#keywords').on('change', function() {
+	$('section').hide();
+	let selectedItem = $('option:selected').val();
+	console.log('1', selectedItem);
+
+	$('option').each(function() {
+		if (selectedItem !== 'undefined') {
+			$(`section[class = "${selectedItem}"]`).show();
+			console.log($(`#${this.text}`));
+		}
+	});
+});
